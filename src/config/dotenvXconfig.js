dffe1118525324be6353002050b06c479e37a3c3
Jorge Betancourt <joisbetancourtes@ittepic.edu.mx>
2025-03-25 20:37:47 -0700

    Proyecto corriendo 25/03/2025

--- src/config/dotenvXconfig.js
+++ src/config/dotenvXconfig.js
@@ -1,7 +1,7 @@
-import dotenvx from "@dotenvx/dotenvx";
+const dotenvx = require("@dotenvx/dotenvx");
 dotenvx.config();
 
-export default {
+module.exports = {
   HOST: process.env.HOST || "No encontré variable localhost",
   PORT: process.env.PORT || "No encontré PORT",
   API_URL: process.env.API_URL || "/api/v1",
